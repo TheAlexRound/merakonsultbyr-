aa9b28bb1e11371b6c1e69286746d6f6e17d825c
AlexRound <alexdkround@gmail.com>
2025-03-31 16:11:59 +0200

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./path/to/your/files/**/*.html",
+      "./path/to/your/files/**/*.js",
+      // Add more paths to your HTML, JS, or Vue components
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
